f9f88fbff5574c66424742c328ce5fff21c3b463
ricardo-banuelos <ricardo.banuelos.se@gmail.com>
2025-08-21 22:00:25 -0700

    made screen fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(
+        sf::VideoMode({1920u, 1080u}), 
+        "CMake SFML Project",
+        sf::State::Fullscreen
+    );
     window.setFramerateLimit(144);
 
     while (window.isOpen())
